e7a3e2118de54472230e2a08e47d26bce71c327b
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-09-24 17:57:55 -0300

    added ex3 test suite class

--- sistema_bancario/src/test/java/negocio/ClasseTeste.java
+++ sistema_bancario/src/test/java/negocio/ClasseTeste.java
@@ -1,6 +1,21 @@
 package negocio;
 
-/* Teste de Classe Aluno XXX */
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Teste de Classe Aluno
+ * 
+ * Kauã Chaves Calixto
+ * Jefferson Ferreira da Silva
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        GerenciadoraClientesTest.class,
+        GerenciadoraContasTest.class
+})
 public class ClasseTeste {
 
 }

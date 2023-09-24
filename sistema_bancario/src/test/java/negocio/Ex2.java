bb40952b3e70dbaf076f87009923a7954f439efb
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-09-24 17:35:08 -0300

    added junit initial tests

---
+++ sistema_bancario/src/test/java/negocio/Ex2.java
@@ -0,0 +1,109 @@
+package negocio;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex2 {
+
+	private GerenciadoraContas gerContas;
+	static final int idConta01 = 1;
+	static final int idConta02 = 2;
+
+//	Saldo suficiente em ambas as contas	
+	@Test
+	public void testiransferevalor01() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, 200, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+
+		// Inserindo os clientes criados no lista de clientes do banco
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		gerContas = new GerenciadoraContas(contasDoBanco);
+		float valorTransferencia = 100.00f;
+		boolean sucesso = gerContas.transfereValor(idConta01, valorTransferencia, idConta02);
+
+		// Delta é a diferença máxima entre os dois valores flutuantes!
+		float delta = 0.0f;
+		assertTrue(sucesso);
+		assertEquals(conta01.getSaldo(), 100.0, delta);
+		assertEquals(conta02.getSaldo(), 100.0, delta);
+	}
+	
+//	Saldo insuficiente e positivo na Conta 1
+	@Test
+	public void testiransferevalor02() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, 100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+
+		// Inserindo os clientes criados no lista de clientes do banco
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		gerContas = new GerenciadoraContas(contasDoBanco);
+		float valorTransferencia = 200.00f;
+		boolean sucesso = gerContas.transfereValor(idConta01, valorTransferencia, idConta02);
+
+		// Delta é a diferença máxima entre os dois valores flutuantes!
+		float delta = 0.0f;
+		assertTrue(sucesso);
+		assertEquals(conta01.getSaldo(), -100.00, delta);
+		assertEquals(conta02.getSaldo(), 200.0, delta);
+	}
+	
+//	Saldo insuficiente e negativo na Conta 1
+	@Test
+	public void testiransferevalor03() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+
+		// Inserindo os clientes criados no lista de clientes do banco
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		gerContas = new GerenciadoraContas(contasDoBanco);
+		float valorTransferencia = 200.00f;
+		boolean sucesso = gerContas.transfereValor(idConta01, valorTransferencia, idConta02);
+
+		// Delta é a diferença máxima entre os dois valores flutuantes!
+		float delta = 0.0f;
+		assertTrue(sucesso);
+		assertEquals(conta01.getSaldo(), -300.00, delta);
+		assertEquals(conta02.getSaldo(), 200.0, delta);
+	}
+	
+//	Saldo insuficiente em ambas as contas
+	@Test
+	public void testiransferevalor04() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, -100, true);
+
+		// Inserindo os clientes criados no lista de clientes do banco
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		gerContas = new GerenciadoraContas(contasDoBanco);
+		float valorTransferencia = 200.00f;
+		boolean sucesso = gerContas.transfereValor(idConta01, valorTransferencia, idConta02);
+
+		// Delta é a diferença máxima entre os dois valores flutuantes!
+		float delta = 0.0f;
+		assertTrue(sucesso);
+		assertEquals(conta01.getSaldo(), -300.00, delta);
+		assertEquals(conta02.getSaldo(), 100.0, delta);
+	}
+
+}
---
+++ sistema_bancario/src/test/java/negocio/GerenciadoraClientesTest.java
@@ -0,0 +1,72 @@
+package negocio;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GerenciadoraClientesTest {
+	
+	private GerenciadoraClientes gerClientes;
+	private int idCliente01 = 1;
+	private int idCliente02 = 2;
+	
+	@Before
+	public void setUp() {
+		
+		/*Montagem do cenário*/
+		
+		//criando alguns clientes
+		Cliente cliente01 = new Cliente(idCliente01, "joão", 31, "joao@gmail.com", 1, true);
+		Cliente cliente02 = new Cliente(idCliente02, "Maria", 34, "maria@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de cliente do banco
+		List<Cliente> clienteDoBanco = new  ArrayList<>();
+		clienteDoBanco.add(cliente01);
+		clienteDoBanco.add(cliente02);
+		
+		gerClientes = new GerenciadoraClientes(clienteDoBanco);
+	}
+	
+	@After
+	public void tearDown() {
+		gerClientes.limpa();
+	}
+	
+	/*
+	 * teste básico da culta de um cliente a partir do seu id
+	 */
+	
+	@Test
+	public void testPesquisaCliente() {
+		/*==========Execução*/
+		Cliente cliente = gerClientes.pesquisaCliente(idCliente01);
+		
+		/*==========Verificação=========*/
+		assertEquals(cliente.getId(), idCliente01);
+	}
+
+	/**
+	 * Teste básico da remoção de um cliente a partir do seu id
+	 * 
+	 */
+	 @Test
+	 public void testRemoverCliente() {
+		 
+		 /*======== Execução ========*/
+		 boolean clienteRemovido = gerClientes.removeCliente(idCliente02);
+		 
+		 /*========Verificação ==========*/
+		 assertTrue(clienteRemovido);
+		 assertEquals(gerClientes.getClientesDoBanco().size(), 1);
+		 assertNull(gerClientes.pesquisaCliente(idCliente02));
+	 }
+	 
+}
